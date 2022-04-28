e37d718558aac67972bac9716b7572c3a6dcd90e
jeahyun <dkswogus9402@naver.com>
2022-04-28 21:51:43 +0900

    04-28 TIL

---
+++ JH/04-26/21611.py
@@ -0,0 +1,145 @@
+N, M = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(N)]
+
+ds_list = [list(map(int, input().split())) for _ in range(M)]
+ # 상어는 항상 제자리네
+
+pos = ((-1,0),(1,0),(0,-1),(0,1))
+result = [0] * 4
+def blizzard(d, s):
+    y, x = N // 2, N // 2
+    # 방향
+    for i in range(1,s+1):
+        ny = y + (pos[d - 1][0] * i)
+        nx = x + (pos[d - 1][1] * i)
+        A[ny][nx] = 0 # 우선 0이라고 표현
+
+
+def reshape(A): # 뽑아내는 과정
+    y, x = N//2, N//2
+    cnt = 0
+    direction = 0
+    move = ((0,-1),(1,0),(0,1),(-1,0))
+    flag = False
+    temp = [] # reshape 하기위한 변수
+    while True:
+        if direction % 2 == 0:
+            cnt += 1
+        if cnt == N:
+            cnt -= 1
+            flag = True
+        for _ in range(cnt):
+            y = y + move[direction][0]
+            x = x + move[direction][1]
+            if A[y][x]: # 0인 것을 제외하고 데이터를 가져옴
+                temp.append(A[y][x])
+        direction = (direction + 1 ) % 4
+        if flag:
+            break
+    # Temp embedding 과정
+    # temp = temp + ([0] * (N * N - len(temp))) # 한개는 상어위치
+    return temp
+
+def reshape_2(temp):    # 채워주는 과정
+    A = [[0] * N for _ in range(N)]
+    y, x = N//2, N//2
+    cnt = 0
+    direction = 0
+    k = 0
+    move = ((0,-1),(1,0),(0,1),(-1,0))
+    flag = False
+    while True:
+        if direction % 2 == 0:
+            cnt += 1
+        if cnt == N:
+            cnt -= 1
+            flag = True
+        for _ in range(cnt):
+            y = y + move[direction][0]
+            x = x + move[direction][1]
+            A[y][x] = temp[k]
+            k += 1
+        direction = (direction + 1 ) % 4
+        if flag:
+            break
+    return A
+
+def boob(temp):
+    global result
+    cnt = 1
+    while True:
+        flag = 0
+        for i in range(len(temp)-1): # 제일 마지막 경우 체크 한번 더
+            if i == len(temp)-2:
+                if temp[i] == temp[i + 1]:
+                    cnt += 1
+                    if cnt > 3:
+                        flag += 1
+                        result[temp[i]] += cnt
+                        for k in range(cnt):
+                            temp[i+1-k] = 0
+                else:
+                    if cnt > 3:
+                        flag += 1
+                        result[temp[i]] += cnt
+                        for k in range(cnt):
+                            temp[i-k] = 0
+                new_temp_ = []
+                for kk in range(len(temp)):
+                    if temp[kk]:
+                        new_temp_.append(temp[kk]) # 0이 아닌것만 뽑아옴
+                temp = new_temp_
+                cnt = 1
+                break
+
+            if temp[i] == temp[i+1]:
+                cnt += 1
+            else:
+                if cnt > 3:
+                    result[temp[i]] += cnt
+                    flag +=1
+                    for k in range(cnt):
+                        temp[i-k] = 0
+                cnt = 1
+
+        if flag == 0:
+            break
+
+    # 구술이 폭발하고 이동한 후
+    new_temp = []
+    cnt = 1
+    for i in range(len(temp) - 1):  # 제일 마지막 경우 체크 한번 더
+        if i == len(temp) - 2:
+            if temp[i] == temp[i + 1]:
+                cnt += 1
+            new_temp.append(cnt)
+            new_temp.append(temp[i])
+
+            if temp[i] != temp[i + 1]:
+                new_temp.append(1)
+                new_temp.append(temp[i+1])
+            break
+
+        if temp[i] == temp[i + 1]:
+            cnt += 1
+        else:
+            new_temp.append(cnt)
+            new_temp.append(temp[i])
+            cnt = 1
+
+    new_temp = new_temp + ([0] * (N * N - len(new_temp)))  # 한개는 상어위치
+    return new_temp
+
+
+
+for d, s in ds_list:
+    blizzard(d, s)
+    temp = reshape(A)
+    if temp == []:
+        break
+    new_temp = boob(temp)
+    A = reshape_2(new_temp)
+
+
+total = result[1] + 2 * result[2] + 3 * result[3]
+print(total)
